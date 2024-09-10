7ddf8b1c21f904ef09fa0deda6a040a74ef0cce1
eug-L <eugene4198@gmail.com>
2024-09-10 16:19:51 +0800

    update copyright

--- Controller/Adminhtml/SelectWidget/Index.php
+++ Controller/Adminhtml/SelectWidget/Index.php
@@ -12,7 +12,7 @@
  * obtain it through the world-wide-web, please send an email
  * to support@tawk.to so we can send you a copy immediately.
  *
- * @copyright   Copyright © 2016 Tawk.to
+ * @copyright   Copyright © 2024 Tawk.to
  * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 
